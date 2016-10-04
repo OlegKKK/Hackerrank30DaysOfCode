24805ee0ee8728e6571c0da6e46cb593a6fdd684
OlegKKK <kramo@ukr.net>
2016-10-04 14:25:13 +0300

    Day 1 Data Types

---
+++ src/DataTypes.java
@@ -0,0 +1,30 @@
+//Day 1: Data Types
+
+import java.util.*;
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+
+        Scanner scan = new Scanner(System.in);
+
+        int x;
+        double y;
+        String z;
+
+        x = scan.nextInt();
+        System.out.println(i+x);
+
+        y = scan.nextDouble();
+        System.out.println(d+y);
+
+        scan.nextLine();
+        z = scan.nextLine();
+        System.out.println(s+z);
+
+        scan.close();
+    }
+}
