db334b8f15c0082b125165396c0f30b3e9e2998a
OlegKKK <kramo@ukr.net>
2016-10-07 16:54:49 +0300

    Day 3 Intro to Conditional Statements

---
+++ src/ConditionalStatements.java
@@ -0,0 +1,27 @@
+//Day 3: Intro to Conditional Statements
+
+import java.util.Scanner;
+
+public class ConditionalStatements {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+        String ans="";
+
+        if(n%2==1){
+            ans = "Weird";
+        }
+        else if(n%2==0 && (n>=6 && n<=20)){
+            ans="Weird";
+        }
+        else if(n%2==0 && (n>=2 && n<=5)){
+            ans="Not Weird";
+        }
+        else if(n%2==0 && (n>20 && n<=100)){
+            ans="Not Weird";
+        }
+        System.out.println(ans);
+    }
+}
