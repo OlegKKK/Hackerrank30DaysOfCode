7dffec5dec2b1a23ebeeaa1e8337cabe2980513c
OlegKKK <kramo@ukr.net>
2016-10-19 11:07:17 +0300

    Day 16 Exceptions - String to Integer

---
+++ src/ExceptionsStringToInteger.java
@@ -0,0 +1,18 @@
+//Day 16: Exceptions - String to Integer
+
+import java.util.Scanner;
+
+public class ExceptionsStringToInteger {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String string = in.next();
+        in.close();
+        try {
+            Integer integer = Integer.parseInt(string);
+            System.out.println(integer);
+        } catch (NumberFormatException numberFormatException) {
+            System.out.println("Bad String");
+        }
+    }
+}
