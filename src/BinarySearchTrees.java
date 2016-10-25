bcb962a59b41e5a5003676faa53a81072c8c6726
OlegKKK <kramo@ukr.net>
2016-10-25 21:00:32 +0300

    Day 22 Binary Search Trees

--- src/BinarySearchTrees.java
+++ src/BinarySearchTrees.java
@@ -17,11 +17,11 @@ class BinarySearchTree {
         return root == null ? -1 : 1 + Math.max(getHeight(root.left), getHeight(root.right));
     }
 
-    public static Node insert(Node root, int data) {
+    public static BinarySearchTree insert(BinarySearchTree root, int data) {
         if (root == null) {
-            return new Node(data);
+            return new BinarySearchTree(data);
         } else {
-            Node cur;
+            BinarySearchTree cur;
             if (data <= root.data) {
                 cur = insert(root.left, data);
                 root.left = cur;
@@ -36,7 +36,7 @@ class BinarySearchTree {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
-        Node root = null;
+        BinarySearchTree root = null;
         while (T-- > 0) {
             int data = sc.nextInt();
             root = insert(root, data);
